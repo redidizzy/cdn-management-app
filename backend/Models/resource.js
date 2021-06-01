3ab5e4b61853ad9a571c64105eb11013f25e473b
Rediane Zemmouri <rediane.zemmouri@skaalab.com>
2021-06-01 21:32:19 +0100

    Indent code

--- backend/Models/resource.js
+++ backend/Models/resource.js
@@ -1,22 +1,24 @@
-const mongoose =require('mongoose') ;
-const schema =mongoose.Schema ;
-const admin = require('./user') ;
-const resourceSchema= new schema(
-{
-name :{
-    type: String ,
-    required :true ,
-    index: { unique: true } },
-url:{
-    type: String ,
-    required :true },
+const mongoose = require('mongoose') ;
+const schema = mongoose.Schema ;
+const admin = require('./user')
+const resourceSchema = new schema({
+    name: {
+        type: String ,
+        required: true ,
+        index: { unique: true } 
+    },
+    url: {
+        type: String ,
+        required: true 
+    },
+    description: {
+        type: String 
+    }, 
+    adminRef: {
+        type: schema.Types.ObjectId, 
+        ref: 'admin' 
+    }
+}, { timestamps: true })
 
-description: {
-    type :String }, 
-adminRef :{
-     type: schema.Types.ObjectId, 
-     ref: 'admin' }
-},{ timestamps:true }) ;
-
-module.exports =mongoose.model("resource", resourceSchema)  ;
+module.exports = mongoose.model("resource", resourceSchema) 
 
--- backend/Models/user.js
+++ backend/Models/user.js
@@ -1,17 +1,17 @@
-const mongoose =require('mongoose') ;
-const schema =mongoose.Schema ;
-const resource  = require('./resource') ;
-const UserSchema= new schema(
-{
-userName :{
-    type: String ,
-    required :true ,
-    index: { unique: true } },
-password:{
-    type: String ,
-    required :true }
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+const resource  = require('./resource')
+const UserSchema= new schema({
+    userName: {
+        type: String,
+        required: true,
+        index: { unique: true } 
+    },
+    password: {
+        type: String,
+        required: true 
+    }
+}, { timestamps: true })
 
-} ,{ timestamps:true }) ;
-
-module.exports =mongoose.model("user", UserSchema)  ;
+module.exports = mongoose.model("user", UserSchema) 
 
