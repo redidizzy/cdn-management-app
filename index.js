416970c4af2f7a0977a8085fa670103727903f6e
Rediane Zemmouri <rediane.zemmouri@skaalab.com>
2021-06-01 21:13:21 +0100

    Use mongoose client instead of legacy mongoclient

--- index.js
+++ index.js
@@ -11,20 +11,16 @@ app.listen(port, () => {
   console.log(`CDN Management System app listening at http://localhost:${port}`)
 })
 //just a db connection and models testing code 
-const MongoClient = require('mongodb').MongoClient ;
-MongoClient.connect('mongodb://localhost:27017/CDN',(err, client) => {
-    if(err) 
-    {
-        return console.error('not connected to mongo') ;
-    }
-app.use(express.urlencoded({extended:true})) ;
-app.get('/' ,(req  ,res)=>{
+mongoose.connect('mongodb://localhost:27017/CDN', {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(express.urlencoded({extended:true}))
+app.get('/' , async (req  ,res)=>{
   //trying to insert a new user 
   //error i get : MongooseError: Operation `users.insertOne()` buffering timed out after 10000ms
- var admin =new user({userName :'nasser' ,password :'123'}) ;
- admin.save(function (err) {
-  if(err) return console.error(err.stack)
-  console.log("user is created")
-      });
-}) ;
-}) ;
+ var admin =new user({userName :'nasser' ,password :'123'})
+ try{
+   await admin.save()
+ }catch(error){
+   console.log(error)
+ }
+}) 
