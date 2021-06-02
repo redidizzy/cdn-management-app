60f50ed584baf21a8c4c69c501d383df0b42c5f0
zekraouin <zekraouin98@gmail.com>
2021-06-02 16:03:24 +0100

    getting all users

---
+++ backend/Routes/userRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const user = require('../Models/user')
+const router =express.Router()
+const resource = require('../Models/user')
+
+
+
+// geting a user 
+router.get('/', async(req,res)=>{
+  try {
+      const users =await user.find()
+      res.send(users)
+      
+  } catch (error) {
+      res.status(500).json({message :error.message})
+  }
+
+})
+//creating a user
+
+//update
+//delete
+module.exports =router
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mongoose = require ('mongoose') ;
 const app = express()
 const port = 3000
-const user =require('./backend/Models/user') ;
 
 // Our routes
 
@@ -10,10 +9,18 @@ const user =require('./backend/Models/user') ;
 app.listen(port, () => {
   console.log(`CDN Management System app listening at http://localhost:${port}`)
 })
-//just a db connection and models testing code 
+//db coneection 
 mongoose.connect('mongodb://localhost:27017/CDN', {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(express.urlencoded({extended:true}))
+
+// handling authentification queries
+const user =require('./backend/Routes/userRoutes') ;
+app.use('/auth',user)
+
+//handling resources requests
+
+
 app.get('/' , async (req  ,res)=>{
   //trying to insert a new user 
  var admin =new user({userName :'nasser' ,password :'123'})
