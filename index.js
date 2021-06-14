7e6b63bd35515c4c5400aa93de2c8f9865d27f35
Rediane Zemmouri <rediane.zemmouri@skaalab.com>
2021-06-14 20:04:02 +0100

    Add auth middleware

--- index.js
+++ index.js
@@ -74,3 +74,20 @@ app.use("/auth", user)
 //handling resources requests
 const resource = require("./backend/Routes/resourceRoutes")
 app.use("/resource", resource)
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
